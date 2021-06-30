3153335a2d52a3b81e7e2eb6ae57d10e3e583557
Anders H <anders.haga@gmail.com>
2021-06-30 02:47:48 +0200

    Update w10_nmnl.ps1

--- w10_nmnl.ps1
+++ w10_nmnl.ps1
@@ -35,6 +35,11 @@ BEGIN
             'Allow notifications to play sounds,HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings,NOC_GLOBAL_SETTING_ALLOW_NOTIFICATION_SOUND,DWord,0'
             'Show notifications on the lock screen,HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings,NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK,DWord,0'
 	    'Suggest ways I can finish setting up my device,HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\UserProfileEngagement,ScoobeSystemSettingEnabled,Dword,0'
+            'Windows Welcome Experience,HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager,SubscribedContent-310093Enabled,DWord,0'
+            'Windows Spotlight,HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager,SubscribedContent-338387Enabled,DWord,0'
+            'Windows App Suggestions,HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager,SubscribedContent-338388Enabled,DWord,0'
+            'Windows Tips and Tricks,HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager,SubscribedContent-338389Enabled,DWord,0'
+	    
 
             # Updates
             'Disable automatic updates,HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU,NoAutoUpdate,DWord,1' # You can still update manually
